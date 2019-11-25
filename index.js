c1fb6ae1816f9527761be48d5adc57648e2240f3
Alina Tylets <alinatylets@gmail.com>
2019-11-25 23:35:03 +0300

    feat: add server and requests middleware logger

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import { router } from "./routes/api/articles.js";
+import { logger } from "./middleware/logger.js";
+
+const app = express();
+const port = 3001;
+
+// Init middleware
+app.use(logger);
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Articles API routes
+app.use("/api/news", router);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ middleware/logger.js
@@ -0,0 +1,10 @@
+import moment from "moment";
+
+export const logger = (req, res, next) => {
+  console.log(
+    `${req.protocol}://${req.get("host")}${
+      req.originalUrl
+    }: ${moment().format()}`
+  );
+  next();
+};
