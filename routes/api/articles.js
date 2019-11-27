7ee42e286fcee27898437a3bd7ae1f92cc916b64
Alina Tylets <alinatylets@gmail.com>
2019-11-27 20:29:50 +0300

    feat: create utils for CRUD operations, simplify router

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,124 +1,28 @@
 import express from "express";
-import fs from "fs";
-import uuid from "uuid";
+
+import {
+  getArticlesUtility,
+  getArticleUtility,
+  createArticleUtility,
+  updateArticleUtility,
+  deleteArticleUtility
+} from "../../utilities/utilities.js";
 
 const router = express.Router();
-const FILENAME = "./news.json";
 
 // Get all articles
-router.get("/", (req, res, next) => {
-  fs.readFile(FILENAME, "utf-8", (err, data) => {
-    if (err) {
-      next(new Error(`NO SUCH FILE (${FILENAME}) FOUND`));
-    }
-    res.type("json").send(data);
-  });
-});
+router.get("/", getArticlesUtility);
 
 // Get single article
-router.get("/:id", (req, res, next) => {
-  fs.readFile(FILENAME, "utf-8", (err, data) => {
-    if (err) {
-      next(`NO SUCH FILE (${FILENAME}) FOUND`);
-    }
-    const parsedData = JSON.parse(data);
-    const found = parsedData.find(article => article.id === +req.params.id);
-
-    if (found) {
-      return res.type("json").send(found);
-    }
-
-    return res.status(400).json({
-      message: `No article with the id of ${req.params.id} was found`
-    });
-  });
-});
+router.get("/:id", getArticleUtility);
 
 // Create article
-router.post("/", (req, res, next) => {
-  const newArticle = {
-    id: uuid.v4(),
-    author: req.body.author,
-    title: req.body.title,
-    description: req.body.description,
-    url: req.body.url,
-    urlToImage: req.body.urlToImage,
-    publishedAt: req.body.publishedAt
-  };
-
-  if (!newArticle.title || !newArticle.url) {
-    return res.status(400).json({ message: "Please include title and url" });
-  }
-
-  fs.readFile(FILENAME, "utf-8", (err, data) => {
-    if (err) {
-      next(`NO SUCH FILE (${FILENAME}) FOUND`);
-    }
-
-    const combinedData = [...JSON.parse(data), newArticle];
-    const stringifiedData = JSON.stringify(combinedData);
-
-    fs.writeFile(FILENAME, stringifiedData, err => {
-      if (err) {
-        next(`NO SUCH FILE (${FILENAME}) FOUND`);
-      }
-      res.status(200).json({ message: "Article added" });
-    });
-  });
-});
+router.post("/", createArticleUtility);
 
 // Update article
-router.put("/:id", (req, res, next) => {
-  fs.readFile(FILENAME, "utf-8", (err, data) => {
-    if (err) {
-      next(`NO SUCH FILE (${FILENAME}) FOUND`);
-    }
-    const parsedData = JSON.parse(data);
-    const foundIndex = parsedData.findIndex(
-      article => article.id === +req.params.id
-    );
-
-    if (foundIndex !== -1) {
-      parsedData[foundIndex] = { ...parsedData[foundIndex], ...req.body };
-      fs.writeFile(FILENAME, JSON.stringify(parsedData), err => {
-        if (err) {
-          next(`NO SUCH FILE (${FILENAME}) FOUND`);
-        }
-        return res.status(200).json({ message: "Article updated" });
-      });
-    } else {
-      res.status(400).json({
-        message: `No article with the id of ${req.params.id} was found`
-      });
-    }
-  });
-});
+router.put("/:id", updateArticleUtility);
 
 // Delete article
-router.delete("/:id", (req, res, next) => {
-  fs.readFile(FILENAME, "utf-8", (err, data) => {
-    if (err) {
-      next(`NO SUCH FILE (${FILENAME}) FOUND`);
-    }
-    const parsedData = JSON.parse(data);
-    const foundIndex = parsedData.findIndex(
-      article => article.id === +req.params.id
-    );
-
-    if (foundIndex !== -1) {
-      parsedData.splice(foundIndex, 1);
-      fs.writeFile(FILENAME, JSON.stringify(parsedData), err => {
-        if (err) {
-          next(`NO SUCH FILE (${FILENAME}) FOUND`);
-        }
-        return res.status(200).json({ message: "Article deleted" });
-      });
-    } else {
-      res.status(400).json({
-        message: `No article with the id of ${req.params.id} was found`
-      });
-    }
-  });
-});
+router.delete("/:id", deleteArticleUtility);
 
 export { router };
---
+++ utilities/utilities.js
@@ -0,0 +1,128 @@
+import fs from "fs";
+import uuid from "uuid";
+
+const FILENAME = "./news.json";
+
+// Get all articles
+const getArticlesUtility = (req, res, next) => {
+  fs.readFile(FILENAME, "utf-8", (err, data) => {
+    if (err) {
+      next(new Error(`NO SUCH FILE (${FILENAME}) FOUND`));
+    }
+    res.type("json").send(data);
+  });
+};
+
+// Get chosen article
+const getArticleUtility = (req, res, next) => {
+  fs.readFile(FILENAME, "utf-8", (err, data) => {
+    if (err) {
+      next(`NO SUCH FILE (${FILENAME}) FOUND`);
+    }
+    const parsedData = JSON.parse(data);
+    const found = parsedData.find(article => article.id === +req.params.id);
+
+    if (found) {
+      return res.type("json").send(found);
+    }
+
+    return res.status(400).json({
+      message: `No article with the id of ${req.params.id} was found`
+    });
+  });
+};
+
+// Create article
+const createArticleUtility = (req, res, next) => {
+  const newArticle = {
+    id: uuid.v4(),
+    author: req.body.author,
+    title: req.body.title,
+    description: req.body.description,
+    url: req.body.url,
+    urlToImage: req.body.urlToImage,
+    publishedAt: req.body.publishedAt
+  };
+
+  if (!newArticle.title || !newArticle.url) {
+    return res.status(400).json({ message: "Please include title and url" });
+  }
+
+  fs.readFile(FILENAME, "utf-8", (err, data) => {
+    if (err) {
+      next(`NO SUCH FILE (${FILENAME}) FOUND`);
+    }
+
+    const combinedData = [...JSON.parse(data), newArticle];
+    const stringifiedData = JSON.stringify(combinedData);
+
+    fs.writeFile(FILENAME, stringifiedData, err => {
+      if (err) {
+        next(`NO SUCH FILE (${FILENAME}) FOUND`);
+      }
+      res.status(200).json({ message: "Article added" });
+    });
+  });
+};
+
+// Update article
+const updateArticleUtility = (req, res, next) => {
+  fs.readFile(FILENAME, "utf-8", (err, data) => {
+    if (err) {
+      next(`NO SUCH FILE (${FILENAME}) FOUND`);
+    }
+    const parsedData = JSON.parse(data);
+    const foundIndex = parsedData.findIndex(
+      article => article.id === +req.params.id
+    );
+
+    if (foundIndex !== -1) {
+      parsedData[foundIndex] = { ...parsedData[foundIndex], ...req.body };
+      fs.writeFile(FILENAME, JSON.stringify(parsedData), err => {
+        if (err) {
+          next(`NO SUCH FILE (${FILENAME}) FOUND`);
+        }
+        return res.status(200).json({ message: "Article updated" });
+      });
+    } else {
+      res.status(400).json({
+        message: `No article with the id of ${req.params.id} was found`
+      });
+    }
+  });
+};
+
+// Delete article
+const deleteArticleUtility = (req, res, next) => {
+  fs.readFile(FILENAME, "utf-8", (err, data) => {
+    if (err) {
+      next(`NO SUCH FILE (${FILENAME}) FOUND`);
+    }
+    const parsedData = JSON.parse(data);
+    const foundIndex = parsedData.findIndex(
+      article => article.id === +req.params.id
+    );
+
+    if (foundIndex !== -1) {
+      parsedData.splice(foundIndex, 1);
+      fs.writeFile(FILENAME, JSON.stringify(parsedData), err => {
+        if (err) {
+          next(`NO SUCH FILE (${FILENAME}) FOUND`);
+        }
+        return res.status(200).json({ message: "Article deleted" });
+      });
+    } else {
+      res.status(400).json({
+        message: `No article with the id of ${req.params.id} was found`
+      });
+    }
+  });
+};
+
+export {
+  getArticlesUtility,
+  getArticleUtility,
+  createArticleUtility,
+  updateArticleUtility,
+  deleteArticleUtility
+};
